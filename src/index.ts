435d3d1dabef6b237599373121bf955f1e86460a
mattiaferrari <mattia2002.ferrari@gmail.com>
2024-06-30 13:52:48 +0200

    type errors correctly

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,19 @@ import createNativeGetRandomValues, {
 
 createNativeGetRandomValues();
 
+// port correct types as the browser/node implementation https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues#exceptions
+class TypeMismatchError extends Error {}
+class QuotaExceededError extends Error {}
+
 const getRandomValues = <T extends CompatibleArray>(array: T): T => {
   if (array.byteLength > 65536) {
-    throw new Error(
+    throw new QuotaExceededError(
       'ArrayBuffer length exceeds maximum length of 65536 bytes.'
     );
   }
   const arrayType = arrayTypeEnum[array.constructor.name];
   if (arrayType === undefined) {
-    throw new Error('Unsupported array type');
+    throw new TypeMismatchError('Unsupported array type');
   }
 
   const filledArray = global.__NativeGetRandomValues.getRandomValues<T>(
@@ -34,4 +38,4 @@ if (typeof global.crypto.getRandomValues === 'undefined') {
   global.crypto.getRandomValues = getRandomValues;
 }
 
-export { getRandomValues };
+export { getRandomValues, TypeMismatchError, QuotaExceededError };
